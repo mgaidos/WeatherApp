0b612a7a0ff63d8fbe13d473c01a9239301e8ce2
mgaidos <m.gaidos>
2023-07-04 10:08:53 +0200

    Box component and some styles

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,12 +1,23 @@
+import React from 'react'
 
+//MUI Components
+import { Box } from '@mui/material'
 
 function App() {
 
 
   return (
-    <>
+    <Box sx={{
+      marginTop: "20px",
+      width: "80vw",
+      maxWidth: 1000,
+      height: "90vh",
+      display: "flex",
+      border: "1px solid black",
+      borderRadius: 5
+      }}>
   
-    </>
+    </Box>
   )
 }
 
